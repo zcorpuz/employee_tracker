38a710056a7eaa710de7d41078308bb224b79af9
Zach Corpuz <zachcorpuz@yahoo.com>
2020-06-24 23:19:15 -0700

    added constructor classes for each database. Created inquirer prommpts for role and employee

--- index.js
+++ index.js
@@ -10,20 +10,104 @@ const connection = mysql.createConnection({
 });
 
 connection.connect(err => {
-    if (err) throw err
+    if (err) throw err;
 });
 
-const startApp = () => {
+const startApp = {
+    name: "action",
+    type: "rawlist",
+    message: "What would you like to do?",
+    choices: [
+        "Add New Department",
+        "Add New Role",
+        "Add New Employee",
+        "View List of Employees",
+        "Update Employee"
+    ]
+};
+
+
+const addDept = () => {
     inquirer
         .prompt({
-            name: "action",
-            type: "rawlist",
-            message: "What would you like to do?",
-            choices: [
-                "Add New Department",
-                "Add New Employee",
-                "View List of Employees",
-                "Update Employee"
-            ]
-        });
-}
+            name: "deptName",
+            type: "input",
+            message: "What is the name of the department that you would like to add?",
+            validate: (answer) => {
+                if (answer.length < 1) {
+                    return 'Please enter a valid department name';
+                }
+                return true;
+            },
+        })
+        // .then(answer => {
+        //     let query = "INSERT INTO department(id, name) VALUES("
+        // })
+};
+
+const addRole = () => {
+    inquirer
+        .prompt({
+            name: "title",
+            type: "input",
+            message: "What is the title of the position you would like to add?",
+            validate: (answer) => {
+                if (answer.length < 1) {
+                    return 'Please enter a valid title';
+                }
+                return true;
+            },
+            name: "salary",
+            type: "input",
+            message: "What is the salary for this position?",
+            validate: (answer) => {
+                if (answer.length < 1) {
+                    return 'Please enter a valid salary';
+                }
+                return true;
+            },
+            name: "deptId",
+            type: "input",
+            message: "What is the department id for this title?",
+            validate: (answer) => {
+                if (answer.length < 1) {
+                    return 'Please enter a valid department id';
+                }
+                return true;
+            },
+        })
+};
+
+const addEmployee = () => {
+    inquirer
+        .prompt({
+            name: "firstName",
+            type: "input",
+            message: "What is the employee's first name?",
+            validate: (answer) => {
+                if (answer.length < 1) {
+                    return 'Please enter a valid first name';
+                }
+                return true;
+            },
+            name: "lastName",
+            type: "input",
+            message: "What is the employee's last name?",
+            validate: (answer) => {
+                if (answer.length < 1) {
+                    return 'Please enter a valid last name';
+                }
+                return true;
+            },
+            name: "roleId",
+            type: "input",
+            message: "What is the employee's role id?",
+            validate: (answer) => {
+                if (answer.length < 1) {
+                    return 'Please enter a valid role id';
+                }
+                return true;
+            },
+        })
+};
+
